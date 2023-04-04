6b4a1fab50d7d31f5af473e55d79fc17c3c7e921
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2023-04-04 10:04:52 -0500

    Add files via upload

---
+++ Expo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 10:01:08 2023
+
+@author: lab
+"""
+
+print(""" ***********  Uso de notas musicales con append  ***********\n""")
+print("  ")
+
+notas_musicales = ["Do", "Re", "Mi", "Fa", "Sol", "La"]
+print("  ")
+
+print("Lista Original append\n")
+
+print(notas_musicales,"\n")
+
+print("Lista incorporada con append\n")
+
+notas_musicales.append("Si")
+
+print(notas_musicales)
