d0648d604606c928deea5a72b72637b1e9f68710
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2023-04-05 10:43:15 -0500

    Add files via upload

---
+++ POO-Diccionarios.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+dict1={"nombre": "Juan Carlos",
+       "apellido":"Domingez",
+       "telefono":"  :0987654345",
+       "email":"jdominguez@ups.edu.ec",
+       5008:"test",
+       5.8:23,
+       True:"estado"
+       }
+"buscar a aprtir de su lave y si no se encuentra mandar un mensaje"
+
+# dict1.get("Gabriela Díaz", "no se encuentra")
+# print(dict1.get("Gabriela Díaz", "no se encuentra"))
+
+"te debuelve una lista con todo las llaves del diccionario"
+k=dict1.keys()
+print(k)
+
+
+
+
+
+# dict1["MATRICULA"]=1
+# dict1["integrantes"]=1
+
+# """
+# print(dict1[True])
+# print(dict1[5.8])
+# print("NIVEL"in dict1)
+
+# """
