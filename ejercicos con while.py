0998d5565aeaa4c8d45b44191e8ad21cb48759b2
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2023-04-11 10:54:13 -0500

    Add files via upload

---
+++ ejercicos con while.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 10:14:57 2023
+
+@author: lab
+"""
+""" Ejercicios con while, consultar while True"""
+# contar = input("El # numero qu ese debe contar: ")
+# contar = int(contar)
+# contador = 1
+
+# while contador <= contar:
+#     print(contador)
+#     contador= contador + 1 
+    
+
+# contar = input("El # numero que se debe contar: ")
+# contar = int(contar)
+# contador = 1
+
+# while True:
+#     print(contador)
+#     contador= contador + 1 
+#     if contador > contar:
+#         break
+    
+
+
+# contar = input("El # numero que se debe contar: ")
+# contar = int(contar)
+# contador = 1
+
+# while True:
+#     print(contador)
+#     contador= contador + 1 
+#     if contador > contar:
+#         break
+    
+# while True:
+#     x = input("Enter a number to count to: ")
+#     if x =="q" or x =="quit":
+#         break
+# x = int(x)
+# y = 1
+# while True:
+#     print(y)
+#     y = y+1
+#     if y>x:
+#         break 
+        
+    
+while True:
+    x = input("Enter a number to count to: ")
+    if x =="q" or x =="quit":
+        break
+    
+    x = int(x)
+    y = 1
+    while True:
+        print(y)
+        y = y+1
+        if y>x:
+            break 
+        
+    
